7b2cf0b26d03c501eb84c77a298477203fdbf5b3
esHakomo <95696227+sgEber@users.noreply.github.com>
2023-06-21 15:19:58 -0500

    Update app.py

--- myvenv/app/app.py
+++ myvenv/app/app.py
@@ -3,19 +3,19 @@ from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-# Configuración de la base de datos
+
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'mini'
 
-# Inicialización de la extensión MySQL
+
 mysql = MySQL(app)
 
 
 @app.route('/')
 def index():
-    # Obtener todos los usuarios de la base de datos
+    
     cur = mysql.connection.cursor()
     cur.execute("SELECT * FROM usuario")
     usuarios = cur.fetchall()
@@ -26,11 +26,11 @@ def index():
 @app.route('/agregar', methods=['GET', 'POST'])
 def agregar():
     if request.method == 'POST':
-        # Obtener los datos enviados en el formulario
+        
         nombre = request.form['nombre']
         email = request.form['email']
 
-        # Insertar el nuevo usuario en la base de datos
+       
         cur = mysql.connection.cursor()
         cur.execute("INSERT INTO usuario (nombre, email) VALUES (%s, %s)", (nombre, email))
         mysql.connection.commit()
@@ -43,17 +43,17 @@ def agregar():
 def editar(id):
     cur = mysql.connection.cursor()
     if request.method == 'POST':
-        # Obtener los datos enviados en el formulario
+      
         nombre = request.form['nombre']
         email = request.form['email']
 
-        # Actualizar los datos del usuario en la base de datos
+      
         cur.execute("UPDATE usuario SET nombre = %s, email = %s WHERE id = %s", (nombre, email, id))
         mysql.connection.commit()
         cur.close()
         return redirect('/')
     else:
-        # Obtener los datos del usuario a editar
+     
         cur.execute("SELECT * FROM usuario WHERE id = %s", (id,))
         usuario = cur.fetchone()
         cur.close()
@@ -62,7 +62,7 @@ def editar(id):
 
 @app.route('/eliminar/<int:id>', methods=['POST'])
 def eliminar(id):
-    # Eliminar el usuario de la base de datos
+
     cur = mysql.connection.cursor()
     cur.execute("DELETE FROM usuario WHERE id = %s", (id,))
     mysql.connection.commit()
